e8a47f2bccee8843dd1e8a986ad355b7161a9c33
Harshad Bhutada <hbhutada@codeaurora.org>
2012-02-22 13:37:45 +0530

    skia: Use neon optimized memset functions
    When the architecture supports NEON, use the neon optimized
    memset routines.
    
    (cherry picked from commit c7246b98a480b06fc207cfcb1a3d209baac06859)
    
    Change-Id: I3011fc02b51eb298f4a18b0ca0371f8c19721d77

--- include/core/SkUtils.h
+++ include/core/SkUtils.h
@@ -40,10 +40,23 @@ typedef void (*SkMemset32Proc)(uint32_t dst[], uint32_t value, int count);
 SkMemset32Proc SkMemset32GetPlatformProc();
 
 #if defined(ANDROID) && !defined(SK_BUILD_FOR_ANDROID_NDK)
-    #include "cutils/memory.h"
-    
-    #define sk_memset16(dst, value, count)    android_memset16(dst, value, (count) << 1)
-    #define sk_memset32(dst, value, count)    android_memset32(dst, value, (count) << 2)
+    #if defined(__ARM_HAVE_NEON)
+        extern "C" void memset16_neon(uint16_t*, uint16_t, int);
+        extern "C" void memset32_neon(uint32_t*, uint32_t, int);
+
+        /*
+         * count is the number of times value should be copied to dst buffer.
+         * In case of memset16_neon, it is the number of half-words and in case
+         * of memset32_neon, it is the number of words.
+         */
+        #define sk_memset16(dst, value, count)    memset16_neon(dst, value, count)
+        #define sk_memset32(dst, value, count)    memset32_neon(dst, value, count)
+    #else
+        #include "cutils/memory.h"
+
+        #define sk_memset16(dst, value, count)    android_memset16(dst, value, (count) << 1)
+        #define sk_memset32(dst, value, count)    android_memset32(dst, value, (count) << 2)
+    #endif
 #endif
 
 #ifndef sk_memset16
