4ee52c9498f54f943a3a5b976ccff3151969fc15
Henrik Smiding <henrik.smiding@stericsson.com>
2012-08-11 23:28:17 +0200

    Add optimization of Skia S32A_Blend blitter
    Adds optimization of Skia S32A_Blend_BlitRow32 blitter using ARM NEON
    instruction set. Special cases for when pixel alpha is zero.
    Improves performance platform wide.
    
    Change-Id: I6c8bf8a9525838682206ebd139855354d6b3a563
    Signed-off-by: Henrik Smiding <henrik.smiding@stericsson.com>
    Signed-off-by: Patrik Ryd <patrik.ryd@stericsson.com>

--- Android.mk
+++ Android.mk
@@ -51,8 +51,8 @@ ifeq ($(ARCH_ARM_HAVE_NEON),true)
 	LOCAL_CFLAGS += -D__ARM_HAVE_NEON
 endif
 
-# Enable Neon assembler optimized version of S32A_Opaque_BlitRow32.
-# Overrides the intrinsic blitter below.
+# Enable Neon assembler optimized version of S32A_Opaque_BlitRow32 and
+# S32A_Blend_Blitrow32. Overrides the intrinsic blitter below.
 LOCAL_CFLAGS += -DENABLE_OPTIMIZED_S32A_BLITTERS
 
 # special checks for alpha == 0 and alpha == 255 in S32A_Opaque_BlitRow32
@@ -269,6 +269,7 @@ ifeq ($(TARGET_ARCH),arm)
 ifeq ($(ARCH_ARM_HAVE_NEON),true)
 LOCAL_SRC_FILES += \
 	src/opts/S32A_Opaque_BlitRow32_neon.S \
+	src/opts/S32A_Blend_BlitRow32_neon.S \
 	src/opts/memset16_neon.S \
 	src/opts/memset32_neon.S
 
