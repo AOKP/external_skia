c51707d711f21817b37c93ff004a09f0e57cc9ed
Krylon360 <b.t.walter@gmail.com>
2012-08-20 04:02:09 -0600

    Skia: Fix path to OpenGL Headers
    The path that Skia was pointing to for the OpenGL Headers
    was moved to frameworks/native. This patch will also be
    submitted to the mainline for Google.
    
    Change-Id: I75d2ce267d5ec202bb2e5e995f25f9ddfba0dc57

--- Android.mk
+++ Android.mk
@@ -417,7 +417,7 @@ LOCAL_C_INCLUDES += \
   $(LOCAL_PATH)/src/core \
   $(LOCAL_PATH)/src/gpu \
   $(LOCAL_PATH)/third_party/glu \
-  frameworks/base/opengl/include
+  frameworks/native/opengl/include
 
 LOCAL_LDLIBS += -lpthread
 
@@ -455,7 +455,7 @@ LOCAL_SHARED_LIBRARIES := \
 LOCAL_C_INCLUDES += \
   $(LOCAL_PATH)/third_party/glu \
   $(LOCAL_PATH)/third_party/glu/libtess \
-  frameworks/base/opengl/include
+  frameworks/native/opengl/include
 
 LOCAL_LDLIBS += -lpthread
 
