289ece91e78e53a446118c5e450d70fed038f24f
Henrik Smiding <henrik.smiding@stericsson.com>
2013-05-02 14:58:39 +0200

    Modify sample buffer size for larger displays.
    Modifies the intermediate buffer size for sample pixel indexes, used in the
    sample proc function calls. If the buffer is not large enough, the job is split
    into several calls. This would impact the performance of SIMD optimizations.
    
    Change-Id: I86848d1c44fe338258b40f6ee6a576289cebd496
    Signed-off-by: Henrik Smiding <henrik.smiding@stericsson.com>
    Signed-off-by: Patrik Ryd <patrik.ryd@stericsson.com>

--- src/core/SkBitmapProcShader.cpp
+++ src/core/SkBitmapProcShader.cpp
@@ -155,7 +155,13 @@ void SkBitmapProcShader::endContext() {
     this->INHERITED::endContext();
 }
 
-#define BUF_MAX     128
+/* Defines the buffer size for sample pixel indexes, used in the sample proc
+ * function calls. If the buffer is not large enough, the job is split into
+ * several calls. This would impact the performance of SIMD optimizations.
+ * A display with a 720p resolution requires a buffer size of at least 361,
+ * to run uninterrupted.
+ */
+#define BUF_MAX     384
 
 #define TEST_BUFFER_OVERRITEx
 
